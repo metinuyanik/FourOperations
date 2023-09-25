b6c0385747a338975da085d1d462cb5fb046c4a6
Metin Uyanik <metinuynk218@gmail.com>
2023-09-25 22:17:54 +0300

    Codes was written.

--- fourOperations/ViewController.swift
+++ fourOperations/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textOne: UITextField!
+    @IBOutlet weak var textTwo: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    var Sum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func additionClick(_ sender: Any) {
+        if let textOne = Int(textOne.text!){
+            if let textTwo = Int(textTwo.text!) {
+                Sum = textOne + textTwo
+                resultsLabel.text = String(Sum)
+            }
+        }else{
+            resultsLabel.text = "Wrong char entry!"
+        }
+        
+    }
+    
+    @IBAction func subtractionClick(_ sender: Any) {
+        if let textOne = Int(textOne.text!){
+            if let textTwo = Int(textTwo.text!) {
+                Sum = textOne - textTwo
+                resultsLabel.text = String(Sum)
+            }
+        }else{
+            resultsLabel.text = "Wrong char entry"
+        }
+        
+    }
+    @IBAction func multiplicationClick(_ sender: Any) {
+        if let textOne = Int(textOne.text!){
+            if let textTwo = Int(textTwo.text!) {
+                Sum = textOne * textTwo
+                resultsLabel.text = String(Sum)
+            }
+        }else{
+            resultsLabel.text = "Wrong char entry"
+        }
+        
+    }
+    @IBAction func divisionClick(_ sender: Any) {
+        if let textOne = Int(textOne.text!){
+            if let textTwo = Int(textTwo.text!) {
+                Sum = textOne / textTwo
+                resultsLabel.text = String(Sum)
+            }
+        }else{
+            resultsLabel.text = "Wrong char entry"
+        }
+        
+    }
 }
 
